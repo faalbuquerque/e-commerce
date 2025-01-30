1540bc4525e4fa6e51272bd7fc6957c5e6263875
Fabiane Albuquerque <bii.nett@gmail.com>
2025-01-30 17:00:41 -0300

    feat: add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :products
-  get "up" => "rails/health#show", as: :rails_health_check
 
-  root "rails/health#show"
+  get :cart, to: 'carts#show'
+  post :cart, to: 'carts#create'
+  delete 'cart/:product_id', to: 'carts#destroy'
+  put 'cart/add_item', to: 'carts#update'
+
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
+  root 'rails/health#show'
 end
